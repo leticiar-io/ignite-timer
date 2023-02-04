665f8c8b5b8ea19e2e9ec66f84eeded36baa2178
leticiar-io <leticia.rodrigues20019@gmail.com>
2023-02-04 12:03:56 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from "./components/Button";
+
+export function App() {
+  return (
+    <>
+      <p>Hello</p>
+
+      <Button variant="primary"/>
+      <Button variant="danger"/>
+
+      <Button variant="success"/>
+      <Button variant="secondary" />
+    </>
+  );
+}
---
+++ src/components/Button.tsx
@@ -0,0 +1,19 @@
+import { ButtonContainer, ButtonVariant } from "./Button.styles"
+
+interface ButtonProps{
+    variant?: ButtonVariant;
+}
+
+const ButtonVariants = {
+    primary: 'purple',
+    secondary: 'orange',
+    danger: 'red',
+    sucess: 'green'
+}
+
+
+export function Button({ variant = "primary"}: ButtonProps){
+    return(
+        <ButtonContainer variant={variant}>Enviar</ButtonContainer>
+    )
+}

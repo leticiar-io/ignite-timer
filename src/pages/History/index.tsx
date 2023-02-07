e73830297ecd63915355b4d905573b89d3d8fbba
leticiar-io <leticia.rodrigues20019@gmail.com>
2023-02-07 07:36:41 -0300

    component: Status

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,4 +1,4 @@
-import { HistoryContainer, HistoryList } from './styles'
+import { HistoryContainer, HistoryList, Status } from './styles'
 
 export function History() {
   return (
@@ -21,25 +21,33 @@ export function History() {
               <td>Task</td>
               <td>20 minutes</td>
               <td>At 2 months</td>
-              <td>Finished</td>
+              <td>
+                <Status statusColor="green">Finished</Status>
+              </td>
             </tr>
             <tr>
               <td>Task</td>
               <td>20 minutes</td>
               <td>At 2 months</td>
-              <td>Finished</td>
+              <td>
+                <Status statusColor="yellow">In progress</Status>
+              </td>
             </tr>
             <tr>
               <td>Task</td>
               <td>20 minutes</td>
               <td>At 2 months</td>
-              <td>Finished</td>
+              <td>
+                <Status statusColor="green">Finished</Status>
+              </td>
             </tr>
             <tr>
               <td>Task</td>
               <td>20 minutes</td>
               <td>At 2 months</td>
-              <td>Finished</td>
+              <td>
+                <Status statusColor="red">Stopped</Status>
+              </td>
             </tr>
           </tbody>
         </table>
